bdac5e7bec281c56ba0d82b1727133f696b55751
Luke Vasilion <lukevasilion@gmail.com>
2025-12-17 00:18:46 -0500

    revert all "fixes"

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from "amplify-adapter";
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
+
